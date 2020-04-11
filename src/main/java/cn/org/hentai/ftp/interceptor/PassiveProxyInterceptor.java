57b7f4564dfefc1b66c1fb70ebed72d7fe19fb46
matrixy <matrixy@me.com>
2020-04-11 20:10:05 +0800

    改为常规PASV模式

--- src/main/java/cn/org/hentai/ftp/interceptor/PassiveProxyInterceptor.java
+++ src/main/java/cn/org/hentai/ftp/interceptor/PassiveProxyInterceptor.java
@@ -4,6 +4,7 @@ import cn.org.hentai.ftp.message.ASCIIMessage;
 import cn.org.hentai.ftp.message.Message;
 import cn.org.hentai.ftp.proxy.PassiveDataTransfer;
 import cn.org.hentai.ftp.util.ByteUtils;
+import cn.org.hentai.ftp.util.Configs;
 import cn.org.hentai.ftp.util.FTPUtils;
 
 /**
@@ -33,7 +34,9 @@ public abstract class PassiveProxyInterceptor extends SimpleSessionInterceptor
 
             logger.debug("Passive port allocate: {} -> {}", upstreamPassivePort, localPassivePort);
 
-            return new ASCIIMessage("229 Entering Extended Passive Mode (|||" + localPassivePort + "|)\r\n");
+            // 上面的数字，1,2,3,4为IP的四个数字，用逗号隔开，后面两个数字为临时端口的高8位和低8位
+            String pasvAddress = Configs.get("ftp.proxy.passive.addr");
+            return new ASCIIMessage("227 Entering Extended Passive Mode (" + pasvAddress.replaceAll("\\.", ",") + "," + ((localPassivePort >> 8) & 0xff) + "," + (localPassivePort & 0xff) + ")\r\n");
         }
         // 关闭数据连接
         else if (text.startsWith("226 "))
